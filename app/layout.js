cdb893fc51966da0452f28419a7cdce6f1d3a8ae
Amit Singh <amit@sundigitalgroup.com>
2024-11-26 22:09:35 +0530

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,15 @@ const RootLayout = ({ children }) => (
     <head>
       <link rel="preconnect" href="https://blissy.tech" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+      {/* Google tag (gtag.js) */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-EZF20XSVDF"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-EZF20XSVDF');
+      </script>
     </head>
     <body>{children}</body>
   </html>
